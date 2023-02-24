fabe221145325b8e2dd4efda0f85a8f5803094b2
danielbmutinda <danielbenjamin254@gmail.com>
2023-02-23 22:53:55 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,14 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
-*print_numbers - print 0123456789
-* owned by Bwave ICT
-*Return: void
-*/
-
+ * print_numbers - prints the numbers from 0 to 9
+ *
+ * Return: numbers
+ */
 void print_numbers(void)
 {
-	char c;
+	int num;
 
-	for (c = '0'; c <= '9'; c++)
-	{
-	_putchar(c);
-	}
+	for (num = 48 ; num <= 57 ; num++)
+		_putchar(num);
 	_putchar('\n');
 }
