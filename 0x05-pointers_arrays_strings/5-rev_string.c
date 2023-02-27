f1e0d47fee1730b65f5001f0e6cc6535f385c142
danielbmutinda <danielbenjamin254@gmail.com>
2023-02-27 10:17:40 -0800

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+  int i, len;
+  s = "My School";
+  len = strlen(s);
+
+  for (i = 0; i <= len; i--)
+    {
+   
