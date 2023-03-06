056bd671f2e19229560bbd336149e43d2145c5c6
danielbmutinda <danielbenjamin254@gmail.com>
2023-03-06 01:58:06 -0800

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,34 +1,45 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * unsigned int _strspn(char *s, char *accept) gets the length of a prefix 
- *substring.
- *
- * Return: Always 0.
- */
+ * _strspn - Locates a character in a string
+ * @s: This is the main C string to be scanned.
+ * @accept: This is the string containing the list of characters to match in s
+ * Return: return count
+ **/
+
 unsigned int _strspn(char *s, char *accept)
-{
-int i, j, f, flag;
 
-f = 0;
+{
+	int i, j;
+	int count = 0;
+	char *str1, *str2;
 
-for (i = 0; s[i] != '\0'; i++)
-{
-flag = 0;
-for (j = 0; accept[j] != '\0'; j++)
-{
-if (s[i] == accept[j])
-{
-f++;
-flag = 1;
-}
-}
-if (flag == 0)
-{
-return (f);
-}
+	str1 = s;
+	str2 = accept;
+
+	i = 0;
+	while (str1[i] != '\0') /*Declaring WHILE *s */
+	{
+		j = 0;
+		while (str2[j] != '\0') /*Declaring WHILE *accept*/
+		{
+			if (str2[j] == str1[i]) /*Evaluate condition*/
+			{
+				count++; /*count number*/
+				break;
+			}
+
+			j++;    /*add j+1*/
+		}
+
+		if (s[i] != accept[j]) /*If aren't equals*/
+		{
+			break;
+		}
+
+		i++; /*add x+1*/
+	}
+
+	return (count); /*return the value of count*/
 }
 
-return (0);
-}
