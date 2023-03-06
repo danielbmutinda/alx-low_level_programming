b6f38d9fcf14ab9565ec9b463fdfb9f036c6eb60
danielbmutinda <danielbenjamin254@gmail.com>
2023-03-06 11:00:44 -0800

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,11 +1,13 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * set_string - sets the value of a pointer to a char
- * @s: pointer to pointer we need to set to
- * @to: string to set
+ * @s: double pointer
+ * @to: pointer
+ *
+ * Return : Void function .
  */
 void set_string(char **s, char *to)
 {
-*s = to;
+	 *s = to;
 }
--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,28 +1,20 @@
 #include "main.h"
 
 /**
- * print_chessboard - prints an array of chars as a chessboard
- * @a: the array to be printed
- * Return: nothing
- */
+* print_chessboard - print chessboard given set 2D array
+* @a: 2D array
+*/
+
 void print_chessboard(char (*a)[8])
 {
-	int row = 0;
-	int col;
 
-	while (row < 8)
-	{
-		col = 0;
-		while (col < 8)
-		{
-			if (col == 7)
-			{
-				_putchar(a[row][col]);
-				_putchar('\n');
-			}
-			else
-				_putchar(a[row][col]);
-			col++;
-		}
-		row++;
-	}
+int row;
+int column;
+
+for (row = 0; row < 8; row++)
+{
+for (column = 0; column < 8; column++)
+_putchar(a[row][column]);
+_putchar('\n');
+}
+}
