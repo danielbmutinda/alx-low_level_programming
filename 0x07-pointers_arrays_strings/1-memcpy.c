e8a7dddc7d0be59399e1fc2d6b7fa4de993d741f
danielbmutinda <danielbenjamin254@gmail.com>
2023-03-06 01:22:43 -0800

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_memcpy - copies memory area.
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int a;
+for (a = 0; a < n; a++)
+{
+dest[a] = src[a];
+}
+return (dest);
+}
