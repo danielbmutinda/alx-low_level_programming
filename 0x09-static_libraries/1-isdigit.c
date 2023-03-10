6bea4acf0c81daaa5d20cec36a10dcdf84cbc711
danielbmutinda <danielbenjamin254@gmail.com>
2023-03-10 04:53:30 -0800

    create_static_lib.sh

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isdigit - check the code for Holberton School students.
+ *
+ * @c: is an integer param
+ *
+ * Return: Always 0.
+ */
+
+
+int _isdigit(int c)
+{
+
+	return (c >= 48 && c <= 57);
+}
