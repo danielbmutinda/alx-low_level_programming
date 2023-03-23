53570ac74fed6b731d2d39357930884cb5afbaee
danielbmutinda <danielbenjamin254@gmail.com>
2023-03-22 21:11:51 -0700

    0-sum_them_all.c

--- 0-main.c
+++ 0-main.c
@@ -1,51 +1,18 @@
-#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
+#include "variadic_functions.h"
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-    unsigned int i;
-
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
-}
-
-/**
- * main - check the code for ALX School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char *buffer;
+    int sum;
 
-    buffer = create_array(98, 'H');
-    if  (buffer == NULL)
-    {
-        printf("failed to allocate memory\n");
-        return (1);
-    }
-    simple_print_buffer(buffer, 98);
-    free(buffer);
+    sum = sum_them_all(2, 98, 1024);
+    printf("%d\n", sum);
+    sum = sum_them_all(4, 98, 1024, 402, -1024);
+    printf("%d\n", sum);    
     return (0);
 }
